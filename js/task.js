f5f1f616a325cb10e37086b886b8c6f41ce91f40
smd <smd78@list.ru>
2019-05-01 23:26:48 +0300

    Это наш пятый комит

--- js/task.js
+++ js/task.js
@@ -13,12 +13,36 @@ let appData = {
     saving: false
 }
 
-let rashInMonth = prompt("Введите обязательную статью расходов в этом месяце","");
-let rashInDay = prompt("Во сколько это обойдется?","");
-let rashInMonth2 = prompt("Введите обязательную статью расходов в этом месяце","");
-let rashInDay2 = prompt("Во сколько это обойдется?","");
+/*for (let i = 0; i <2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце","");
+    let b = prompt("Во сколько это обойдется?","");
 
-appData.expenses.rashInMonth = rashInDay;
-appData.expenses.rashInMonth2 = rashInDay2;
+    if(typeof(a) === 'string' && typeof(a) !=null &&  typeof(b) !=null && a !='' && b !='' && a.length > 50){
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
 
-alert(appData.budget/30);
+    }
+};*/
+let i = 0;
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце","");
+    let b = prompt("Во сколько это обойдется?","");
+
+    if(typeof(a) === 'string' && typeof(a) !=null &&  typeof(b) !=null && a !='' && b !='' && a.length > 50){
+        console.log('done');
+        appData.expenses[a] = b;
+    }
+    i++;
+}
+while (i < 2);
+appData.moneyPerDay = appData.budget/30;
+alert('Ежедневный бюджет: '+ appData.moneyPerDay);
+
+if (appData.moneyPerDay <100){
+    console.log('Минимальный уровень достатка')
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log('средний уровень достатка')
+} else if (appData.moneyPerDay > 2000){
+    console.log('Высокий уровень достатка')
+}
